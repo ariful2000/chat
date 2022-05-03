93a847a4cf0421abd04bcaab40114c8a6cf3cf13
ariful2000 <57025439+ariful2000@users.noreply.github.com>
2022-05-03 14:31:52 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,19 @@
-import 'package:chat_app/screens/login_screen.dart';
+import 'package:chatt_app/Authenticate/Authenticate.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-Future<void> main() async {
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        
-        primarySwatch: Colors.blue,
-      ),
-      home: const LoginScreen(),
+      debugShowCheckedModeBanner: false,
+      home: Authenticate(),
     );
   }
 }
-
